bb11f2f2f3a363573e9ff682975787005dc2c220
Dave Kennedy <david.kennedy@ubc.ca>
2018-05-10 13:26:37 -0700

    fix for: Strict warning: Declaration of CRM_Core_Payment_CBM::singleton() should be compatible with & CRM_Core_Payment::singleton(, &, & = NULL,  = false)

--- CRM/Core/Payment/CBM.php
+++ CRM/Core/Payment/CBM.php
@@ -61,7 +61,7 @@ class CRM_Core_Payment_CBM extends CRM_Core_Payment {
      * @return object 
      * @static 
      * */
-	static function &singleton($mode, &$paymentProcessor) {
+	static function &singleton($mode = 'test', &$paymentProcessor, &$paymentForm = NULL, $force = FALSE) {
 		$processorName = $paymentProcessor['name'];
 		if (self::$_singleton[$processorName] === NULL) {
 		  self::$_singleton[$processorName] = new CRM_Core_Payment_CBM($mode, $paymentProcessor);
